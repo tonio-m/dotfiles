2799001149ba60107b1d7819f339c6a4ee1a0f4e
tonio <anntonio@protonmail.ch>
2021-01-19 17:28:20 -0300

    removed cool screen lock script
    back to using i3lock

--- .config/i3/screen_lock.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-rm -f /tmp/screen_lock_screenshot.png && \
-scrot /tmp/screen_lock_screenshot.png && \
-python ${HOME}/.config/i3/pixel_displace.py /tmp/screen_lock_screenshot.png && \
-i3lock -i /tmp/screen_lock_screenshot.png
-
-
