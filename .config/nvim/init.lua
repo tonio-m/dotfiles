b7f47039cac01304bb2df11bd1131298ea77d3c1
mvieiraTAG <141847029+mvieiraTAG@users.noreply.github.com>
2024-07-31 18:16:10 -0300

    lotsa stuff

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,20 @@
 require("config.lazy")
-require("lazy").setup("plugins")
-require('onedark').setup()
+require("commands")
+require("languageserver")
+require("functions")
 
-vim.g.markdown_fenced_languages = {'json', 'python', 'html', 'javascript', 'bash'}
+require("onedark").setup()
+require("nvim-tree").setup({hijack_netrw = true})
+require('llm').setup({
+    timeout_ms = 10000,
+    services = {
+        openai = {
+            model = "gpt-4o",
+            api_key_name = "OPENAI_API_KEY",
+            url = "https://api.openai.com/v1/chat/completions",
+        },
+    }
+})
 
 vim.cmd('syntax on')
 vim.cmd('hi Folded ctermbg=none')
@@ -29,16 +41,37 @@ vim.o.foldmethod = 'indent'
 vim.o.relativenumber = true
 vim.g.netrw_browse_split = 4
 vim.g.netrw_maxfilenamelen = 66
-vim.o.clipboard = 'unnamedplus'
 vim.o.backspace = 'indent,eol,start'
+vim.opt.clipboard:append('unnamedplus')
+vim.g.calendar_action = 'CalendarFunction'
+vim.g.markdown_fenced_languages = {'json', 'python', 'html', 'javascript', 'bash'}
 
-vim.api.nvim_set_keymap('n', 'Y', 'y$', { noremap = true, silent = false })
-vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('t', '<Esc><Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '*', 'y/\\V<C-R>=escape(@",\'/\\\')<CR><CR>', { noremap = true, silent = false })
+
+-- vim.keymap.set('n', '<leader>wn', ':Wn<CR>', { noremap = true, silent = true })
+-- vim.keymap.set('n', '<leader>mn', ':Mn<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
+vim.keymap.set('n' , 'gd', vim.lsp.buf.definition, {})
+vim.keymap.set('n', 'Y', 'y$', { noremap = true, silent = false })
+vim.keymap.set('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>dn', ':Dn<CR>', { noremap = true, silent = true })
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+vim.keymap.set('v', '*', 'y/\\V<C-R>=escape(@",\'/\\\')<CR><CR>', { noremap = true, silent = false })
+
+-- llm.nvim
+vim.keymap.set("n", "<leader>g,", function() require("llm").prompt({ replace = false, service = "openai" }) end, { desc = "Prompt with openai" })
+vim.keymap.set("v", "<leader>g,", function() require("llm").prompt({ replace = false, service = "openai" }) end, { desc = "Prompt with openai" })
+vim.keymap.set("v", "<leader>g.", function() require("llm").prompt({ replace = true, service = "openai" }) end, { desc = "Prompt while replacing with openai" })
+
+-- calendar stuff
+vim.keymap.set('n', '<leader>cal', ':CalendarH<CR>', { noremap = true, silent = true })
+
+-- keymap for shift gf to do  :vsp | edit <cfile> 
+vim.keymap.set('n', 'gF', ':vsp | edit <cfile><CR>', {noremap = true, silent = true})
+
 vim.api.nvim_create_autocmd('TermOpen', {
     pattern = '*',
     callback = function()
@@ -46,84 +79,10 @@ vim.api.nvim_create_autocmd('TermOpen', {
         vim.opt_local.relativenumber = false
     end,
 })
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'netrw',
+    callback = function()
+        vim.api.nvim_buf_set_keymap(0, 'n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
+    end,
+})
 
-vim.api.nvim_create_user_command('Sterm', function()
-  vim.cmd('sp | terminal')
-end, {})
-vim.api.nvim_create_user_command('Vterm', function()
-  vim.cmd('vsp | terminal')
-end, {})
-
-vim.api.nvim_create_user_command('In', function()
-  vim.cmd('Explore $HOME/Obsidian/marco_vault/000_inbox/')
-end, {})
-
-vim.api.nvim_create_user_command('Va', function()
-  vim.cmd('Explore $HOME/Obsidian/marco_vault/003_vault/')
-end, {})
-
-vim.api.nvim_create_user_command('Jo', function()
-  vim.cmd('Explore $HOME/Obsidian/marco_vault/001_journal/')
-end, {})
-
-vim.api.nvim_create_user_command('Dn', function()
-  local date = os.date("%Y-%m-%d")
-  vim.cmd('edit $HOME/Obsidian/marco_vault/001_journal/' .. date .. '.md')
-end, {})
-
-vim.api.nvim_create_user_command('SetTabSize', function(args)
-  local n = tonumber(args.args)
-  vim.opt_local.tabstop = n
-  vim.opt_local.shiftwidth = n
-  vim.opt_local.softtabstop = n
-end, { nargs = 1 })
-
-vim.api.nvim_create_user_command('SortByWidth', function(opts)
-  local line1 = opts.line1
-  local line2 = opts.line2
-  local command = string.format(':%d,%d! awk \'{ print length(), $0 | "sort -n | cut -d\\\\  -f2-" }\'', line1, line2)
-  vim.cmd(command)
-end, { range = true })
-
-vim.api.nvim_create_user_command('Nn', function(params)
-  local current_time = os.date("%Y-%m-%dT%H:%M:%S%z")
-  local args = params.args
-  if #args == 0 then
-    vim.cmd('edit $HOME/Obsidian/marco_vault/000_inbox/' .. current_time .. '.md')
-  else
-    local filename = args .. '.md'
-    vim.cmd('edit $HOME/Obsidian/marco_vault/000_inbox/' .. filename)
-  end
-end, { nargs = '*'})
-
-vim.api.nvim_create_user_command('Wn', function(params)
-  offset = tonumber(params.args) or 0
-  local week_num = tonumber(os.date('%V')) + offset
-  local year = tonumber(os.date('%Y'))
-  
-  if week_num < 1 then
-    week_num = 52 + week_num
-    year = year - 1
-  elseif week_num > 52 then
-    week_num = week_num - 52
-    year = year + 1
-  end
-
-  local filename = string.format('/Users/user/Obsidian/marco_vault/001_journal/%d-W%02d.md', year, week_num)
-  if vim.fn.filereadable(filename) == 0 then
-    local week_offset = offset * 604800  -- 7 days in seconds
-    local days = {"sun", "mon", "tue", "wed", "thu", "fri", "sat"}
-    local content = {}
-
-    for i, day in ipairs(days) do
-      local time_offset = (i - tonumber(os.date('%w'))) * 86400 + week_offset
-      table.insert(content, day .. ":")
-      table.insert(content, string.format('[[001_journal/%s.md]]', os.date('%Y-%m-%d', os.time() + time_offset)))
-      table.insert(content, "")
-    end
-
-    vim.fn.writefile(content, filename)
-  end
-
-  vim.cmd('edit ' .. filename)
-end, { nargs = '?' })
--- .zshrc
+++ .zshrc
@@ -1,4 +1,9 @@
 source $HOME/.config/scripts/minimal.zsh
+
+if [ -f ~/.env ]; then
+  source ~/.env
+fi
+
 source /opt/homebrew/opt/chruby/share/chruby/chruby.sh
 source /opt/homebrew/opt/chruby/share/chruby/auto.sh
 chruby ruby-3.2.2
@@ -10,29 +15,30 @@ export do='--dry-run=client -o=yaml'
 export DOCKER_DEFAULT_PLATFORM=linux/amd64
 export PATH="/Users/user/.local/bin/:$PATH"
 export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+export OBSIDIAN_HOME="$HOME/Obsidian/marco_vault/"
 
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 
 alias vim=nvim
-alias k=kubectl
+# alias k=kubectl
 alias make=gmake
-alias i="cd $HOME/Obsidian/marco_vault/000_inbox/ && vim -c ':Explore .'"
-alias v="cd $HOME/Obsidian/marco_vault/003_vault/ && vim -c ':Explore .'"
+# alias i="cd $HOME/Obsidian/marco_vault/000_inbox/ && vim -c ':Explore .'"
+# alias v="cd $HOME/Obsidian/marco_vault/003_vault/ && vim -c ':Explore .'"
 
-dn() {
-    local offset=${1:--0} #default for $1 is -0
-    vim "$HOME/Obsidian/marco_vault/001_journal/$(date -v${offset}d +"%Y-%m-%d").md"
-}
+# dn() {
+#     local offset=${1:--0} #default for $1 is -0
+#     vim "$HOME/Obsidian/marco_vault/001_journal/$(date -v${offset}d +"%Y-%m-%d").md"
+# }
 
-nn() {
-    cd "$HOME/Obsidian/marco_vault/000_inbox/"
-    if [ $# -eq 0 ]; then
-      vim "$(date +"%Y-%m-%dT%H:%M:%S%z").md"
-    else
-      vim "$*.md"
-    fi
-}
+# nn() {
+#     cd "$HOME/Obsidian/marco_vault/000_inbox/"
+#     if [ $# -eq 0 ]; then
+#       vim "$(date +"%Y-%m-%dT%H:%M:%S%z").md"
+#     else
+#       vim "$*.md"
+#     fi
+# }
 
 add_worktree() {
     if [ -z "$1" ]; then
@@ -51,36 +57,50 @@ add_worktree() {
     cd ../${branch_name}
 }
 
-week() {
-    cd $HOME/Obsidian/marco_vault
-    local file="001_journal/$(date +%Y-W%V).md"
-    if [ -e ${file} ]; then
-        vim ${file}
-    else
-        echo """sun:
-[[001_journal/$(date -vsun +"%Y-%m-%d").md]]
+# week() {
+#     cd $HOME/Obsidian/marco_vault
+#     local file="001_journal/$(date +%Y-W%V).md"
+#     if [ -e ${file} ]; then
+#         vim ${file}
+#     else
+#         echo """sun:
+# [[001_journal/$(date -vsun +"%Y-%m-%d").md]]
+# 
+# mon: 
+# [[001_journal/$(date -vmon +"%Y-%m-%d").md]]
+# 
+# tue:
+# [[001_journal/$(date -vtue +"%Y-%m-%d").md]]
+# 
+# wed:
+# [[001_journal/$(date -vwed +"%Y-%m-%d").md]]
+# 
+# thu:
+# [[001_journal/$(date -vthu +"%Y-%m-%d").md]]
+# 
+# fri:
+# [[001_journal/$(date -vfri +"%Y-%m-%d").md]]
+# 
+# sat:
+# [[001_journal/$(date -vsat +"%Y-%m-%d").md]]
+# """ | vim -c "file ${file}" -
+#     fi
+# }
 
-mon: 
-[[001_journal/$(date -vmon +"%Y-%m-%d").md]]
-
-tue:
-[[001_journal/$(date -vtue +"%Y-%m-%d").md]]
-
-wed:
-[[001_journal/$(date -vwed +"%Y-%m-%d").md]]
-
-thu:
-[[001_journal/$(date -vthu +"%Y-%m-%d").md]]
-
-fri:
-[[001_journal/$(date -vfri +"%Y-%m-%d").md]]
-
-sat:
-[[001_journal/$(date -vsat +"%Y-%m-%d").md]]
-""" | vim -c "file ${file}" -
-    fi
-}
+# c() {
+#   local content="$*"
+#   curl https://api.openai.com/v1/chat/completions \
+#     -H "Content-Type: application/json" \
+#     -H "Authorization: Bearer $OPENAI_API_KEY" \
+#     -d '{
+#       "model": "gpt-3.5-turbo",
+#       "messages": [{"role": "user", "content": "'"${content}"'"}],
+#       "temperature": 0.7
+#     }' | jq -r '.choices[0].message.content'
+# }
 
 alias apa="export AWS_PROFILE=amherst"
-alias s3r="aws s3 ls --recursive --human-readable"
+# alias s3r="aws s3 ls --recursive --human-readable"
 alias s3="aws s3 ls --human-readable"
+alias git_undo="git reset --soft HEAD~1"
+alias cdv="cd ~/.config/nvim"
