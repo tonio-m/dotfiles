5e6afc6a8bb3e72e4fc38ff7603a043d417ca326
user <user@users-MacBook-Pro.local>
2024-10-12 09:43:19 -0300

    feat: add support for <links> to files in gobllm chat

--- .config/nvim/lua/gobllm.lua
+++ .config/nvim/lua/gobllm.lua
@@ -1,10 +1,16 @@
 -- NOTE: Idea stolen from the great @VictorTaelin and melbaldove/llm.nvim
--- t his is my cute rewrite
+-- this is my cute rewrite
 local M = {}
 local vim = vim or {}
 local curl = require('plenary.curl')
 local CHAT_SYSTEM_PROMPT = [[
-You are an AI programming assistant. Follow the user's requirements carefully & to the letter. You are an expert on software development. Keep your answers short and impersonal. First think step-by-step - describe your plan for what to build. Then output the code in a single code block. Minimize any other prose. Use Markdown formatting in your answers. Make sure to include the programming language name at the start of the Markdown code blocks. Avoid wrapping the whole response in triple backticks. The user works in an IDE called Neovim. You can only give one reply for each conversation turn.
+You are an AI programming assistant. Follow the user's requirements carefully & to the letter. 
+You are an expert on software development. Keep your answers short and impersonal. 
+First think step-by-step. Then output the code in a single code block. Minimize any other prose. 
+Use Markdown formatting in your answers. 
+Make sure to include the programming language name at the start of the Markdown code blocks. 
+Avoid wrapping the whole response in triple backticks. The user works in an IDE called Neovim. 
+You can only give one reply for each conversation turn.
 ]]
 local USER_COMPLETION_MARKER = "<"..">"
 local INTERNAL_COMPLETION_MARKER = "{{" .. "FILL HERE" .. "}}"
@@ -12,10 +18,9 @@ local DEFAULT_SYSTEM_PROMPT = [[
 You're a code completion assistant. 
 You're an expert in generating blocks of code.
 You are provided with a file containing holes, formatted as ']] .. INTERNAL_COMPLETION_MARKER .. [['. 
-Write ONLY the needed text to replace ]] .. INTERNAL_COMPLETION_MARKER ..[[ with the correct completion, including correct spacing and indentation. 
-Include the answer inside a <COMPLETION></COMPLETION> tag.
-All completions MUST be truthful, accurate, well-written and correct.
-Think step by step.
+Write ONLY the needed text to replace ]] .. INTERNAL_COMPLETION_MARKER ..[[ with the correct completion, 
+including correct spacing and indentation. Include the answer inside a <COMPLETION></COMPLETION> tag.
+All completions MUST be truthful, accurate, well-written and correct. Think step by step.
 ]]
 
 function M.split_into_lines(str)
@@ -44,28 +49,49 @@ end
 function M.chat()
     local current_buffer = vim.fn.bufnr('%')
     local buffer_str = table.concat(vim.api.nvim_buf_get_lines(current_buffer, 0, -1, false),"\n")
-
     local messages = M.parse_chat(buffer_str)
+    for i, message in ipairs(messages) do
+        if message.role == "user" then
+            local expanded_text = M.replace_file_links(messages[i].content)
+            messages[i].content = expanded_text
+        end
+    end
     table.insert(messages,1,{role = "system", content = CHAT_SYSTEM_PROMPT})
-
     local answer = M.completion_request(messages)
     local line_count = vim.api.nvim_buf_line_count(current_buffer)
     vim.api.nvim_buf_set_lines(current_buffer, line_count, line_count, false, M.split_into_lines("### A:\n" .. answer .. "\n### Q:"))
+end
+
+function M.read_file(filepath)
+    local file = io.open(filepath, "r")
+    if not file then
+        return nil, "Could not open file: " .. filepath
+    end
+    local content = file:read("*all")
+    file:close()
+    return content
+end
+
+function M.replace_file_links(text)
+    local result = "\n" .. text
+    for match in result:gmatch("\n<[^>]+>") do
+        local filepath = match:sub(3, -2)
+        local file_contents = M.read_file(filepath)
+        result = result:gsub(match, "\n```".. filepath .."\n".. file_contents .. "```")
+    end
+    return result
 end
 
 function M.parse_chat(text)
     local messages= {}
     local content = ""
     local role = nil
-
     local lines = M.split_into_lines(text)
-
     for i, line in ipairs(lines) do
         if (line:match("^### Q:%s*") or  line:match("^### A:%s*") or i  == #lines) and i ~= 1 then
             table.insert(messages,{role = role, content = content})
             content = ""
         end
-
         if line:match("^### Q:%s*")  then
             role = "user"
         elseif line:match("^### A:%s*") then
@@ -87,7 +113,7 @@ function M.completion_request(messages)
             model = M.config.model,
             messages = messages,
         }),
-        timeout = 30000
+        timeout = 60000
     })
     if response.status ~= 200 then
         vim.notify("HTTP ERROR:" .. response.status)
@@ -119,3 +145,4 @@ function M.setup(opts)
 end
 
 return M
+
